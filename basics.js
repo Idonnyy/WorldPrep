234adae83ba5e6e69002b6a7cf2f8d228b650b37
Thomas <othdonov@amazon.com>
2026-02-04 10:44:26 -0800

    Initial commit

---
+++ basics.js
@@ -0,0 +1,25 @@
+// Variables
+let age = 25;
+const name = "Donovan";
+
+//Function
+function greet(user) {
+    return "Hello, " + user;
+}
+
+//Conditionals
+function canVote(age) {
+    if (age >= 18) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+// Loops
+function countToFive() {
+    for (let i = 1; i <=5; i++) {
+        console.log(i);
+    }
+}
